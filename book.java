26f4dc6435a4c86b3a50392c280145659c86cff0
SHUBHAM7796 <shub7796@gmail.com>
2024-02-26 16:43:28 +0530

    bug 121 fixed

--- book.java
+++ book.java
@@ -1,5 +1,13 @@
 // this is for git practice
+<<<<<<< Updated upstream
 public void m2(){
 	//logic 
 	System.out.println("Bug 122 fixed");
 }
+=======
+
+public void m1(){
+	//logic
+	System.out.println("Bug 121 fixed")
+}
+>>>>>>> Stashed changes
